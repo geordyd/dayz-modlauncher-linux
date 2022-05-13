0b2399e8e56664619299996a42d68cab64d4625f
geordyd <geordycjd@gmail.com>
2022-05-13 22:37:30 +0200

    fixed search

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,47 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import Menu from './components/Menu/Menu';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Menu from "./components/Menu/Menu";
 function App() {
+    const [searchValue, setSearchValue] = useState("");
+    const [serverList, setServerList] = useState(Object);
+    const [filter, setFilter] = useState("");
 
-  const [searchValue, setSearchValue] = useState('test');
-  const [serverList, setServerList] = useState(Object);
-  const [filter, setFilter] = useState(false);
+    useEffect(() => {
+        let value = filter;
 
+        if (value !== "") {
+            value = `"${filter}"`;
+        }
 
-  useEffect(() => {
-    fetch(`https://api.battlemetrics.com/servers?filter[game]=dayz&filter[search]="${filter}"`).then(res => res.json()).then(data => {
-      setServerList(data);
-      console.log(data)
-    });
-  }, [filter]);
+        fetch(
+            `https://api.battlemetrics.com/servers?filter[game]=dayz&filter[search]=${value}`
+        )
+            .then((res) => res.json())
+            .then((data) => {
+                setServerList(data);
+                console.log(data);
+                console.log(filter);
+            });
+    }, [filter]);
 
-  return (
-    <div className="wrapper">
-      <div className="menu">
-        <Menu searchValue={searchValue} setSearchValue={setSearchValue} setFilter={setFilter}/>
-      </div>
-      <div className="browser">
-        {serverList['data'].map((data:any) => {
-          return <div>{data['attributes']['name']}</div>
-        })}
-      </div>
-      <div className="console">
-        
-      </div>  
-    </div>
-  );
+    return (
+        <div className="wrapper">
+            <div className="menu">
+                <Menu
+                    searchValue={searchValue}
+                    setSearchValue={setSearchValue}
+                    setFilter={setFilter}
+                />
+            </div>
+            <div className="browser">
+                {serverList.data !== undefined &&
+                    serverList["data"].map((data: any) => {
+                        return <div>{data["attributes"]["name"]}</div>;
+                    })}
+            </div>
+            <div className="console"></div>
+        </div>
+    );
 }
 
 export default App;
--- frontend/src/components/Menu/Menu.tsx
+++ frontend/src/components/Menu/Menu.tsx
@@ -1,14 +1,18 @@
 import React from "react";
 
-
-
 const Menu = (props:any) => {
+    const handleKeyDown = (event:any) => {
+        if (event.key === 'Enter') {
+          props.setFilter(props.searchValue);
+        }
+    }
+      
     return (
         <div className="menu">
             <h1>Filters</h1>
             <div></div>
             <h2>Search:</h2>
-            <input type="text" onChange={e => props.setSearchValue(e.target.value)}>
+            <input type="text" onChange={e => props.setSearchValue(e.target.value)} onKeyDown={handleKeyDown}>
             </input>
             <button onClick={() => props.setFilter(props.searchValue)}>
                 Search
@@ -16,5 +20,6 @@ const Menu = (props:any) => {
         </div>
     );
 }
+
 
 export default Menu;
