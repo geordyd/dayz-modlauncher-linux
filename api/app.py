f2a001bb1d524b2d28f4addc74a0b45e71316e76
geordyd <geordycjd@gmail.com>
2022-05-14 18:18:06 +0200

    Added subscribing and unsubscribing from mods to backend

--- api/app.py
+++ api/app.py
@@ -31,38 +31,23 @@ def GetInstalledMods():
     return jsonify({"data": list(subFolders)})
 
 
-# def SubscribeItem(modID):
-#     steamworks.Workshop.SubscribeItem(modID)
+@app.route("/subscribemod/<modid>", methods=['GET'])
+def SubscribeMod(modid):
+    steamworks.Workshop.SetItemSubscribedCallback(SubscribeModItem)
+    steamworks.Workshop.SubscribeItem(int(modid))
+    return f"Subscribed to {modid}"
 
-# def UnsubscribeItem(modID):
-#     steamworks.Workshop.UnsubscribeItem(modID)
 
-# res = requests.get('https://api.battlemetrics.com/servers?filter[search]="193.25.252.34')
+@app.route("/unsubscribemod/<modid>", methods=['GET'])
+def UnsubscribeMod(modid):
+    steamworks.Workshop.SetItemUnsubscribedCallback(UnsubscribeModItem)
+    steamworks.Workshop.UnsubscribeItem(int(modid))
+    return f"Unsubscribed from {modid}"
 
-# response = json.loads(res.text)
 
-# mods = response['data'][0]['attributes']['details']['modIds']
+def SubscribeModItem(modid):
+    steamworks.Workshop.SubscribeItem(modid)
 
-# def RunDayz():
-#     subprocess.run(["steam", "-applaunch", "221100", "-connect=195.82.158.110:11500" "-nolauncher" "-world=empty" "-name=marco"])
 
-# while True:
-#     print("1: Subscribe\n2: Unsubscribe\n3: Run DayZ\n4: Exit")
-#     choice = input("Enter a command: ")
-#     if choice == "1":
-#         for mod in mods:
-#             if steamworks.Workshop.SetItemSubscribedCallback(SubscribeItem):
-#                 SubscribeItem(mod)
-#                 print("Subscribed to " + str(mod))
-#     elif choice == "2":
-#         for mod in mods:
-#             if steamworks.Workshop.SetItemUnsubscribedCallback(UnsubscribeItem):
-#                 UnsubscribeItem(mod)
-#                 print("Unsubscribed from " + str(mod))
-#     elif choice == "3":
-#         RunDayz()
-#         exit()
-#     elif choice == "4":
-#         exit()
-#     else:
-#         print("Invalid command")
+def UnsubscribeModItem(modid):
+    steamworks.Workshop.UnsubscribeItem(modid)
